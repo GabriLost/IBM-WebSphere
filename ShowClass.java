6df6ade1033a4df71dfa8d342f0dfe1a822e915c
GabriLost <parannoic@gmail.com>
2015-07-14 00:28:12 +0300

    Create ShowClass.java

---
+++ ShowClass.java
@@ -0,0 +1,68 @@
+package lab.classes;
+
+public class ShowClass {
+private String gos=null;
+private String kodd = null;
+private String vc = null;
+private String vc_names = null;
+private String kodg = null;
+private String snazvd = null;
+private String pr_nyaz = null;
+private String nazvd = null;
+private String vc_vidstvc = null;
+public String getUst() {
+	return gos;
+}
+public void setgos(String gos) {
+	this.gos = gos;
+}
+public String getKodd() {
+	return kodd;
+}
+public void setKodd(String kodd) {
+	this.kodd = kodd;
+}
+public String getKodg() {
+	return kodg;
+}
+public void setKodg(String kodg) {
+	this.kodg = kodg;
+}
+public String getNazvd() {
+	return nazvd;
+}
+public void setNazvd(String nazvd) {
+	this.nazvd = nazvd;
+}
+public String getSnazvd() {
+	return snazvd;
+}
+public void setSnazvd(String snazvd) {
+	this.snazvd = snazvd;
+}
+public String getVc() {
+	return vc;
+}
+public void setVc(String vc) {
+	this.vc = vc;
+}
+public String getVc_names() {
+	return vc_names;
+}
+public void setVc_names(String vc_names) {
+	this.vc_names = vc_names;
+}
+public String getPr_nyaz() {
+	return pr_nyaz;
+}
+public void setPr_nyaz(String pr_nyaz) {
+	this.pr_nyaz = pr_nyaz;
+}
+public String getVc_vidstvc() {
+	return vc_vidstvc;
+}
+public void setVc_vidstvc(String vc_vidstvc) {
+	this.vc_vidstvc = vc_vidstvc;
+}
+
+}
